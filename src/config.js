ecbbc87b75da6486313d4276161a1898c4228b90
umayanga <umayangag@gmail.com>
2022-03-21 18:06:50 +0530

    v0.0.9

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-const api = require("../../../src/server");
-const app = require("../../../src/routes");
+const api = require("../../../../src/server");
+const app = require("../../../../src/routes");
 
 export const AppRoutes = app.AppRoutes;
 export const ApiRoutes = api.ApiRoutes;
