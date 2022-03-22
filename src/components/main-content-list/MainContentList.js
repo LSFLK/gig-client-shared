3b905bab653bab6c36a0fe4fc4fa117e6e698932
umayanga <umayangag@gmail.com>
2022-03-22 13:40:44 +0530

    view no result found message

--- src/components/main-content-list/MainContentList.js
+++ src/components/main-content-list/MainContentList.js
@@ -10,27 +10,26 @@ class MainContentList extends Component {
 
     render() {
         const {classes, listItems, vertical} = this.props;
+        if (listItems.length === 0) {
+            return <Typography component="p" style={{textAlign: 'center'}}>
+                No Results Found
+            </Typography>
+        }
         if (listItems) {
             return (
                 <List className={classes.listContainer}>
-                    {Array.isArray(listItems) ?
-                        listItems.map((entity) => (
-                            <MainContentItem key={entity.title}
-                                             imageUrl={entity.image_url} title={entity.title}
-                                             subtitle={entity.source_date !== "0001-01-01T00:00:00Z" ?
-                                                 Moment(entity.source_date).format('DD  MMM YYYY h:mm A') :
-                                                 Moment(entity.updated_at).format('DD  MMM YYYY h:mm A')}
-                                             description={entity.snippet}
-                                             links={entity.links.slice(0, 20)}
-                                             categories={entity.categories}
-                                             vertical={vertical}
-                            />
-                        ))
-                        :
-                        <Typography component="p" style={{textAlign: 'center'}}>
-                            No Results Found
-                        </Typography>
-                    }
+                    {listItems?.map((entity) => (
+                        <MainContentItem key={entity.title}
+                                         imageUrl={entity.image_url} title={entity.title}
+                                         subtitle={entity.source_date !== "0001-01-01T00:00:00Z" ?
+                                             Moment(entity.source_date).format('DD  MMM YYYY h:mm A') :
+                                             Moment(entity.updated_at).format('DD  MMM YYYY h:mm A')}
+                                         description={entity.snippet}
+                                         links={entity.links.slice(0, 20)}
+                                         categories={entity.categories}
+                                         vertical={vertical}
+                        />
+                    ))}
                 </List>
             )
         }
