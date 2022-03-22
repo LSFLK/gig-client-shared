babb779d605faeb254151ed91f922b6dc825c3c4
umayanga <umayangag@gmail.com>
2022-03-22 11:38:38 +0530

    define API routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,16 +3,27 @@ const app = require("../../../../../src/routes");        //parent project root d
 export const AppRoutes = app.AppRoutes;
 
 export const ApiRoutes = {
-    tokenValidation: 'api/token/validate',
     images: 'images',
+    tokenValidation: 'api/token/validate',
     login: 'api/user/login',
-    delete: 'api/delete',
-    update: 'api/update',
+    status: 'api/status/',
     search: 'api/search?query=',
     entity: 'api/get/',
-    status: 'api/status/',
     links: 'api/links/',
     relations: 'api/relations/',
+    add: '/api/add',
+    add_batch: '/api/add-batch',
+    delete: 'api/delete',
+    update: 'api/update',
+    terminate: '/api/terminate',
+    upload: '/api/upload',
+    normalize_location: '/api/normalize/location',
+    normalize_name: '/api/normalize/name',
+    normalize: '/api/normalize',
+    user_register: '/api/user/register',
+    reader_register: '/api/reader/register',
+    publish_twitter: '/api/publish/twitter'
+
 };
 
 export function getServerUrl(url) {
