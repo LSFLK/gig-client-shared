84b4deaa14faefa1a88acb5a7a419f8ec2c9d18d
umayanga <umayangag@gmail.com>
2022-03-24 10:46:01 +0530

    fix register function

--- src/auth/Register.js
+++ src/auth/Register.js
@@ -1,16 +1,16 @@
 import {setAuthToken, setAuthUser, setUserRole} from "./User";
 import {ApiRoutes, getServerUrl} from "../routes/Routes";
 
-export async function readerRegister(username, password) {
-    if (username === "" || password === "") {
-        return {error: "username/password required!"};
+export async function readerRegister(email, password) {
+    if (email === "" || password === "") {
+        return {error: "email/password required!"};
     }
 
     let loginUrl = getServerUrl(ApiRoutes.readerRegister);
     const requestOptions = {
         headers: {'Content-Type': 'application/json'},
         method: 'POST',
-        body: JSON.stringify({username: username, password: password})
+        body: JSON.stringify({email: email, password: password})
     };
     let response = await fetch(loginUrl, requestOptions);
     let result = await response.json();
