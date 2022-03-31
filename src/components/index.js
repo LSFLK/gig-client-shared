3cf2f6deb60314ef64b8a0f2130423c1b3fd0ecf
umayanga <umayangag@gmail.com>
2022-03-31 16:01:11 +0530

    define search result object

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export {default as FormattedContentViewer} from "./formatted-content-viewer/index";
 export {default as InfiniteList} from "./infinite-list/index";
 export {default as MainContentList} from "./main-content-list/index";
+export const SearchResultObject = {searchKey: '', result: null, nextPage: 1};
--- src/functions/api/getResults.js
+++ src/functions/api/getResults.js
@@ -3,7 +3,6 @@ import {generateSearchQuery} from "../generateSearchQuery";
 export async function getResults(searchParam, apiRoute, page = 1, limit = 15) {
     let searchUrl = generateSearchQuery(searchParam, apiRoute);
     searchUrl += '&limit=' + limit + '&page=' + page;
-    console.log(searchUrl);
     const response = await fetch(searchUrl, {method: 'GET'});
 
     if (response?.status !== 200) {
